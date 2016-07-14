3d7a39449f93b83d25d57f5afa861ec27bfed530
jarlehansen <jarle@jarlehansen.net>
2016-07-14 22:28:15 +0200

    moved formatted values method to AbstractSmsStrategy

--- src/main/java/no/rogfk/sms/strategy/AbstractSmsStrategy.java
+++ src/main/java/no/rogfk/sms/strategy/AbstractSmsStrategy.java
@@ -1,9 +1,12 @@
 package no.rogfk.sms.strategy;
 
+import no.rogfk.sms.strategy.dto.FormattedValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class AbstractSmsStrategy implements SmsStrategy {
 
@@ -19,6 +22,19 @@ public abstract class AbstractSmsStrategy implements SmsStrategy {
 
     public <T> T sendSms(String url, Class<T> clazz) {
         return restTemplate.getForObject(url, clazz);
+    }
+
+    protected List<FormattedValue> getFormattedValues(List<String> values) {
+        return values.stream().map(url -> {
+            int queryParamStart = url.indexOf("?");
+            if(queryParamStart > 0) {
+                String host = url.substring(0, queryParamStart);
+                String[] queryParams = url.substring(queryParamStart + 1).split("&");
+                return new FormattedValue(host, queryParams);
+            } else {
+                return new FormattedValue(url);
+            }
+        }).collect(Collectors.toList());
     }
 
 }
--- src/main/java/no/rogfk/sms/strategy/ManualQueueStrategy.java
+++ src/main/java/no/rogfk/sms/strategy/ManualQueueStrategy.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Slf4j
 public class ManualQueueStrategy extends AbstractSmsStrategy {
@@ -34,16 +33,7 @@ public class ManualQueueStrategy extends AbstractSmsStrategy {
     }
 
     public List<FormattedValue> getFormattedValues() {
-        return smsQueue.stream().map(url -> {
-            int queryParamStart = url.indexOf("?");
-            if(queryParamStart > 0) {
-                String host = url.substring(0, queryParamStart);
-                String[] queryParams = url.substring(queryParamStart + 1).split("&");
-                return new FormattedValue(host, queryParams);
-            } else {
-                return new FormattedValue(url);
-            }
-        }).collect(Collectors.toList());
+        return super.getFormattedValues(getQueue());
     }
 
     public void emptyQueue() {
